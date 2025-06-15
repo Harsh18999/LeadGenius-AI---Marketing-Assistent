33ac50c13eb851581ecc9ce7859fbeffc4c03a8e
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2025-06-15 08:45:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config:
     SECRET_KEY = 'xyz'  # Keep this safe!
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER = 'uploads'
-    TOGETHER_API_KEY = 'e5591203825e37070d6117175cb3bd247871954000e6a17659a65833bd33105f'
+    TOGETHER_API_KEY = os.environ['TOGETHER_API_KEY']
     
     # Email configuration
     SMTP_SERVER = 'smtp.gmail.com'
     SMTP_PORT = 587
     EMAIL_ADDRESS = 'kumarh18999@gmail.com'
-    EMAIL_PASSWORD = 'hkqt csvi zwhi jvyy'
+    EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 
     @staticmethod
     def init_app(app):
